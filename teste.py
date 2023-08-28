6b4169e2fcf2059a4d41efa484d97a0a47b0f3b2
jussiermiguel <jussier.miguel08@gmail.com>
2023-08-28 17:29:11 -0300

    primeiro dia

---
+++ teste.py
@@ -0,0 +1,19 @@
+"""
+    assim se faz um comentário em linhas
+"""
+# e assim se faz um comentário em uma única linha
+
+"""
+type casting
+numero = '10'
+
+# para fazer o casting para inteiro basta colocá-lo na classe 'int'
+numero2 = int(numero)
+
+print(type(numero))
+print(type(numero2))
+"""
+
+"""
+print e input
+"""
